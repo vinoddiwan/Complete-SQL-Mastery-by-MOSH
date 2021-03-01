ab3b1a8c488589167991f233e3618f5f9f2f8d73
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-03-01 20:09:53 +0530

    Update Subqueries.sql

--- Writing-Complex-Queries/Subqueries.sql
+++ Writing-Complex-Queries/Subqueries.sql
@@ -1,1 +1,12 @@
+USE sql_store
+
+-- find products that are more expensive than Lettuce (id=3)
+SELECT *
+FROM products
+WHERE unitprice > (
+      SELECT unit_price
+      FROM products
+      WHERE product_id = 3
+      );
+
 
