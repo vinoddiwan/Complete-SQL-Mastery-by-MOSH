a9f1f55d564c07d56f5a47befc5afe6ad40ed353
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-03-01 21:07:18 +0530

    Update Subqueries.sql

--- Writing-Complex-Queries/Subqueries.sql
+++ Writing-Complex-Queries/Subqueries.sql
@@ -1,5 +1,4 @@
-
---USE sql_store
+USE sql_store; -- DB
 
 -- find products that are more expensive than Lettuce (id=3)
 SELECT *
@@ -9,5 +8,35 @@ WHERE unitprice > (
       FROM products
       WHERE product_id = 3
       );
+
+
+-- EXERCISE
+-- IN sql_hr database, find employees who earn more than average
+USE sql; -- DB
+
+SELECT *
+FROM employees
+WHERE salary > (SELECT AVG(salary) FROM employees);
+
+
+
+-- Writing Subqueries Using IN Operator
+USE sql_store;
+
+-- Find the products that have never been ordered
+-- observe ordered_items and products table
+SELECT *
+FROM products
+WHERE product_id NOT IN (
+      SELECT DISTINCT product_id
+      FROM order_items
+      );
+
+
+
+
+
+
+
 
 
