372f03d7f174d9089d4c7b8133576177d44a4cc5
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-03-03 15:49:30 +0530

    Update file.sql

--- 3. Retrieving Data From Multiple Tables/file.sql
+++ 3. Retrieving Data From Multiple Tables/file.sql
@@ -1,1 +1,31 @@
+SELECT * FROM orders;
+SELECT * FROM customers;
+
+-- Retrieving data of customers who orders using JOIN
+SELECT * 
+FROM orders
+INNER JOIN customers
+	ON orders.customer_id = customers.customer_id;
+
+-- limiting columns
+SELECT order_id, first_name, last_name 
+FROM orders
+INNER JOIN customers
+	ON orders.customer_id = customers.customer_id
+
+-- Using Alias
+SELECT order_id, o.customer_id, first_name
+FROM orders o
+INNER JOIN customers c
+	ON o.customer_id = c.customer_id;
+
+------------------------------------------------------
+-- Exercise
+SELECT * FROM order_items;
+SELECT * FROM products;
+
+
+------------------------------------------------------
+
+
 
