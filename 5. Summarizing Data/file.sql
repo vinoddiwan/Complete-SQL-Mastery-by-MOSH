739d34888ae0d3bb6b427462de448c1d5b57a65e
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-03-04 15:43:53 +0530

    Update file.sql

--- 5. Summarizing Data/file.sql
+++ 5. Summarizing Data/file.sql
@@ -1,1 +1,28 @@
 
+                      -- Group By
+SELECT SUM(invoice_total) AS total_sales FROM invoices  -- 2590.60
+
+
+-- what if we want to total sales of each client
+SELECT 
+	  client_id, (invoice_total) AS total_sales 
+FROM invoices
+GROUP BY client_id;   -- Group By
+
+
+-- apply more filters
+SELECT SUM(invoice_total) AS total_sales FROM invoices;
+SELECT 
+	client_id, (invoice_total) AS total_sales 
+FROM invoices
+GROUP BY client_id     -- Group By
+ORDER BY total_sales DESC;
+
+                  -- Group By Multiple columns and join
+SELECT 
+	city, 
+  state,
+	client_id, (invoice_total) AS total_sales 
+FROM invoices
+JOIN clients USING(client_id)
+GROUP BY state, city;
