0299673dffd2f629511b27c99db0bd357ffae64b
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-03-07 00:46:22 +0530

    Update file.sql

--- 2. Retrieving Data From A Single Table/file.sql
+++ 2. Retrieving Data From A Single Table/file.sql
@@ -1,1 +1,33 @@
+-- AND, OR and NOT operator
+SELECT *
+FROM customers 
+WHERE birthdate > '1990-01-01' AND points > 1000;
+
+SELECT *
+FROM customers 
+WHERE birthdate > '1990-01-01' OR points > 1000;
+
+SELECT *
+FROM customers
+WHERE birth_date > '1990-01-01' 
+	    OR points > 1000
+      AND state = 'VA';
+-- or
+SELECT *
+FROM customers
+WHERE birth_date > '1990-01-01' OR
+	    (points > 1000 AND state = 'VA');
+
+SELECT *
+FROM customers
+WHERE NOT (birth_date > '1990-01-01' OR points > 1000);
+-- or
+SELECT *
+FROM customers
+WHERE birth_date <= '1990-01-01' AND points <= 1000;
+
+-- Excercise
+SELECT * FROM order_item 
+WHERE order_id =6
+      AND unit_price*quantity > 30;
 
